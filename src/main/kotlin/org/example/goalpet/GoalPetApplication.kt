d4307297b3aaacf7b718a611ea77d0e5816bcd92
damir2407 <dbaltabaev8708@gmai.com>
2024-06-20 17:16:01 +0300

    init

---
+++ src/main/kotlin/org/example/goalpet/GoalPetApplication.kt
@@ -0,0 +1,11 @@
+package org.example.goalpet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoalPetApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoalPetApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/goalpet/GoalPetApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.goalpet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoalPetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
